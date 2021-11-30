ce3ec24a704282aa4318a92e078d5d85b331ed31
donggeun <geeneve@naver.com>
2021-11-30 19:06:57 +0900

    add: logging 파일추가, 코드 정리

--- strategy.py
+++ strategy.py
@@ -1,12 +1,13 @@
 import random
-
+from os import system
+from logging import *
 
 class Strategy:
     def __init__(self):
         self.COOPERATE = 1  # 협력
         self.BETRAY = 0  # 배반
         self.strategies = [
-            self.ticfortat,
+            self.titfortat,
             self.always_betray,
             self.always_cooperate,
             self.grudge,
@@ -14,43 +15,79 @@ class Strategy:
             self.joss,
             self.random_select,
         ]
+        self.name = [
+            "TfT",
+            "난세",
+            "황금률",
+            "FM",
+            "Pb",
+            "Joss",
+            "Rand",
+        ]
+        self.full_name = [
+            "Tit-for-Tat",
+            "난세",
+            "황금률",
+            "FriedMan (Grudger)",
+            "Prober",
+            "Joss",
+            "Random"
+        ]
+        self.strategy_index = [
+            "titfortat",
+            "always_betray",
+            "always_cooperate",
+            "grudge",
+            "prober",
+            "joss",
+            "random_select"
+        ]
 
-    def ticfortat(self, match, is_betrayed, previous_player_select):
+    def titfortat(self, match, is_betrayed, previous_player_select, strategy_number, total_round):
         if match == 1:
             return self.COOPERATE
         return previous_player_select
 
-    def always_betray(self, match, is_betrayed, previous_player_select):
+    def always_betray(self, match, is_betrayed, previous_player_select, strategy_number, total_round):
         return self.BETRAY
 
-    def always_cooperate(self, match, is_betrayed, previous_player_select):
+    def always_cooperate(self, match, is_betrayed, previous_player_select, strategy_number, total_round):
         return self.COOPERATE
 
-    def grudge(self, match, is_betrayed, previous_player_select):
+    def grudge(self, match, is_betrayed, previous_player_select, strategy_number, total_round):
         if match == 1:
             return self.COOPERATE
         if is_betrayed:
             return self.BETRAY
         return self.COOPERATE
 
-    def prober(self, match, is_betrayed, previous_player_select):
+    def prober(self, match, is_betrayed, previous_player_select, strategy_number, total_round):
         if match == 1 or match == 3 or match == 4:
             return self.COOPERATE
         elif match == 2:
             return self.BETRAY
         else:  # 5라운드 이후
             if is_betrayed:
-                return self.ticfortat(match, is_betrayed, previous_player_select)
+                return self.titfortat(match, is_betrayed, previous_player_select, strategy_number, total_round)
             else:
                 return self.BETRAY
 
-    def joss(self, match, is_betrayed, previous_player_select):
+    def joss(self, match, is_betrayed, previous_player_select, strategy_number, total_round):
         if random.randrange(1, 11) == 1:
             return self.BETRAY
-        return self.ticfortat(match, is_betrayed, previous_player_select)
+        return self.titfortat(match, is_betrayed, previous_player_select, strategy_number, total_round)
 
-    def random_select(self, match, is_betrayed, previous_player_select):
+    def random_select(self, match, is_betrayed, previous_player_select, strategy_number, total_round):
         return random.randrange(2)
 
-    def player(self, match, is_betrayed, previous_player_select):
-        return int(input("배반: 0, 협력: 1 >> "))
+    def player(self, match, is_betrayed, previous_player_select, strategy_number, total_round):
+        ok = True
+        while ok:
+            select = input(f"TOTAL {total_round} ROUND\n# ROUND {match}\n배반: 0, 협력: 1 >> ")
+            system("clear")
+            log_by_number(strategy_number, "상대방의 전략: ")
+            log_credit()
+            if select != "0" and select != "1":
+                print("잘못 입력하셨습니다.\n")
+                continue
+            return int(select)
--- util.py
+++ util.py
@@ -5,7 +5,11 @@ BOARD = [[[0, 0], [3, -1]], [[-1, 3], [2, 2]]]
 PLAYER_ONE = 0
 PLAYER_TWO = 1
 
-strategy_class = Strategy()
+s = Strategy()
+strategies = s.strategies
+strategies_name = s.name
+strategies_index = s.strategy_index
+
 
 
 def calculate(p1_select, p2_select):
@@ -15,21 +19,21 @@ def calculate(p1_select, p2_select):
 
 
 def battle(
-    mode, round, p1_strategy, p2_strategy, score_board, p1_index, p2_index, is_log
+    mode, round, p1_strategy, p2_strategy, score_board, p1_index, p2_index, is_log, strategy_number
 ):
     p1_previous = None
     p2_previous = None
     p1_is_betrayed = False
     p2_is_betrayed = False
     for i in range(1, round + 1):
-        p1_select = p1_strategy(i, p2_is_betrayed, p2_previous)
-        p2_select = p2_strategy(i, p1_is_betrayed, p1_previous)
+        p1_select = p1_strategy(i, p2_is_betrayed, p2_previous, strategy_number, round)
+        p2_select = p2_strategy(i, p1_is_betrayed, p1_previous, strategy_number, round)
         calculated_score = calculate(p1_select, p2_select)
         score_board[p1_index] += calculated_score[PLAYER_ONE]
         score_board[p2_index] += calculated_score[PLAYER_TWO]
-        if p1_select == strategy_class.BETRAY:
+        if p1_select == s.BETRAY:
             p1_is_betrayed = True
-        if p2_select == strategy_class.BETRAY:
+        if p2_select == s.BETRAY:
             p2_is_betrayed = True
         p1_previous = p1_select
         p2_previous = p2_select
@@ -37,8 +41,8 @@ def battle(
             battle_log(
                 mode,
                 i,
-                p1_strategy.__name__.capitalize()[0:6],
-                p2_strategy.__name__.capitalize()[0:6],
+                "YOU",
+                strategies_name[strategies_index.index(p2_strategy.__name__)],
                 p1_select,
                 p2_select,
                 score_board[p1_index],
@@ -53,22 +57,22 @@ def minmax_calculate(mode, round, p1, p2_strategy, score_board, p1_index, p2_ind
     p2_is_betrayed = False
     for i in range(1, round + 1):
         p1_select = p1[i - 1]
-        p2_select = p2_strategy(i, p1_is_betrayed, p1_previous)
+        p2_select = p2_strategy(i, p1_is_betrayed, p1_previous, 0, round)
         calculated_score = calculate(p1_select, p2_select)
         score_board[p1_index] += calculated_score[PLAYER_ONE]
         score_board[p2_index] += calculated_score[PLAYER_TWO]
-        if p1_select == strategy_class.BETRAY:
+        if p1_select == s.BETRAY:
             p1_is_betrayed = True
-        if p2_select == strategy_class.BETRAY:
+        if p2_select == s.BETRAY:
             p2_is_betrayed = True
         p1_previous = p1_select
         p2_previous = p2_select
 
 
 def convert_select(select):
-    if select == strategy_class.BETRAY:
-        return "BETRAY"
-    return "COOP"
+    if select == s.BETRAY:
+        return "배반"
+    return "협력"
 
 
 def battle_log(
